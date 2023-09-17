7f90f89426d7a2ad0de0d2ff416c526179fd4230
Basliel Tegegn <basgolilo5@gmail.com>
2023-09-17 08:32:31 -0700

    adding main.h

--- main.h
+++ main.h
@@ -1,9 +1,44 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
 
-int _printf(const char *format, ...);
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/* count length */
+int count_len(const char *);
+
+/* print char */
+int print(char *);
+
+/* printf.c */
+int _printf(const char *, ...);
+
+/* handler.c */
+int handler(const char *, va_list);
+int percent_handler(const char *, va_list, int *);
+
+/* printers */
+int print_string(va_list);
+int print_char(va_list);
+
+
+/* _putchar.c */
+int _putchar(char);
+int buf_c(char);
+
+/**
+ * struct _format - Typedef struct
+ *
+ * @type: Format
+ * @f: The function associated
+ **/
+typedef struct _format
+{
+	char type;
+	int (*f)(va_list);
+} format;
+
 
 #endif
+
