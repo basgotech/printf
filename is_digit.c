cd6774c19a432ed32c339dd30a810c271854d7db
fraolketema <138703893+fraolketema@users.noreply.github.com>
2023-09-19 13:51:46 +0300

    Update is_digit.c

--- is_digit.c
+++ is_digit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * is_digit - Verifies if a char is a digit
- * @c: Char to be evaluated
+ * is_digit - Verifies if a char is a digit.
+ * @c: Char to be evaluated.
  *
- * Return: 1 if c is a digit, 0 otherwise
+ * Return: 1 if c is a digit, 0 otherwise.
  */
 int is_digit(char c)
 {
