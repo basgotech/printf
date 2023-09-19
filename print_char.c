d4e6e0514565472b2d4bc0d114729312e0e66c20
fraolketema <138703893+fraolketema@users.noreply.github.com>
2023-09-19 13:54:06 +0300

    Update print_char.c

--- print_char.c
+++ print_char.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_char - Prints a char
+ * print_char - Prints a char.
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flags:  Calculates active flags.
  * @width: Width
  * @precision: Precision specification
  * @size: Size specifier
