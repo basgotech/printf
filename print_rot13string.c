ca27f87864f1c15a736909249ad39ec479604a8b
fraolketema <138703893+fraolketema@users.noreply.github.com>
2023-09-19 13:59:08 +0300

    Update print_rot13string.c

--- print_rot13string.c
+++ print_rot13string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_rot13string - Print a string in rot13.
- * @types: Lista of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width
